9a7340ca94159f82e57dc86a5f23fe698150502d
bglid <benjamin.h.glidden@gmail.com>
2026-08-16 08:42:17 -0400

    removing data dir and evaluation script for library simplificaiton

--- tests/conftest.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-import pytest
-
-from adaptive_filter.filter_models.filter_model import FilterModel
--- tests/test_main.py
+++
@@ -1,6 +0,0 @@
-import pytest
-
-from adaptive_filter.evaluation import full_evaluation
-
-# arg parsing module
-from adaptive_filter.utils import arg_parsing
