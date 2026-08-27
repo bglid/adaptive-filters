efaab1ac72d138a541e76c1ba1e1694c43b26bd7
elnaske <naske.elias@outlook.com>
2026-08-27 11:49:23 +0200

    Implement Error trait for FilterError

--- rust/src/errors.rs
+++ rust/src/errors.rs
@@ -1,3 +1,4 @@
+use std::error::Error;
 use std::fmt::Display;
 
 pub type FilterResult<T> = Result<T, FilterError>;
@@ -18,21 +19,22 @@ pub enum FilterError {
 impl Display for FilterError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match *self {
-            Self::EmptyInputArr => write!(f, "Empty input array."),
+            Self::EmptyInputArr => write!(f, "empty input array."),
             Self::NoiseRefTooShort {
                 input_len,
                 noise_len,
             } => write!(
                 f,
-                "Length of the noise reference ({noise_len}) must equal to or greater than that of the input signal ({input_len})."
+                "length of the noise reference ({noise_len}) must be equal to or greater than that of the input signal ({input_len})."
             ),
             Self::WeightSizeMismatch {
                 weight_len,
                 buffer_len,
             } => write!(
                 f,
-                "Number of weights ({weight_len}) and samples ({buffer_len}) don't match."
+                "number of weights ({weight_len}) and samples ({buffer_len}) don't match."
             ),
         }
     }
 }
+impl Error for FilterError {}
